2c286816caad5aa09dbefeccf23a57d7cdcde6ea
CatXer <jsergey01@mail.ru>
2020-02-03 17:23:09 +0300

    New fulscreen loading

--- app/src/main/java/com/gagagus/MainActivity.kt
+++ app/src/main/java/com/gagagus/MainActivity.kt
@@ -2,13 +2,16 @@ package com.gagagus
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.webkit.*
 import android.widget.Button
 import android.widget.Toast
 import android.widget.ViewFlipper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlinx.android.synthetic.main.main_container.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +22,102 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewFlipper: ViewFlipper
 
 
+    private val mHideHandler = Handler()
+    private val mHidePart2Runnable = Runnable {
+        // Delayed removal of status and navigation bar
+
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        fullscreen_content.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
+    private val mShowPart2Runnable = Runnable {
+        // Delayed display of UI elements
+        supportActionBar?.show()
+    }
+
+    private var mVisible: Boolean = false
+    private val mHideRunnable = Runnable { hide() }
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+
+
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        mVisible = false
+
+        //window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable)
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    private fun show() {
+        // Show the system bar
+        window.statusBarColor =
+            ContextCompat.getColor(applicationContext, R.color.colorAccent)
+        fullscreen_content.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        mVisible = true
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable)
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    /**
+     * Schedules a call to hide() in [delayMillis], canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int) {
+        mHideHandler.removeCallbacks(mHideRunnable)
+        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private val AUTO_HIDE = true
+
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private val UI_ANIMATION_DELAY = 300
+    }
+
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mVisible = true
+
+
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_container)
 
@@ -44,9 +141,12 @@ class MainActivity : AppCompatActivity() {
 
         webView.webViewClient = object : WebViewClient() {
 
+
             override fun onPageFinished(view: WebView?, url: String?) {
+
                 if (attachWeb) {
                     viewFlipper.displayedChild = 1
+                    show()
                     attachWeb = false
                 }
                 webLayout.isRefreshing = false
@@ -59,6 +159,7 @@ class MainActivity : AppCompatActivity() {
             ) {
                 attachWeb = false
                 webView.visibility = View.INVISIBLE
+                hide()
                 viewFlipper.displayedChild = 2
             }
 
@@ -103,6 +204,7 @@ class MainActivity : AppCompatActivity() {
         else
             webView.loadUrl(resources.getString(R.string.url))
 
+        hide()
         viewFlipper.displayedChild = 0
     }
 }
